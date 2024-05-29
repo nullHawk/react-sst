176f5783353e8361fc2c80e9c58f50a5d17852c2
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2024-05-29 10:16:02 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Effect from './components/Effect/Effect';
 import Products from './components/Products/Products';
+import NavBar from './components/NavBar/NavBar';
 // import {a,b} from './Products'; // destructuring in JS
 function App() {
   return (
     <div className="App">
+      <header>
+        <NavBar />
+      </header>
       <Effect />
       <Products />
     </div>
