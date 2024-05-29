b71cc563160acaec4ea2c49a04116435773a2a36
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2024-05-29 12:32:38 +0530

    added typewriter effect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Effect from './components/Effect/Effect';
 import Products from './components/Products/Products';
 import NavBar from './components/NavBar/NavBar';
+import TypeWriter from './components/Effect/TypeWriter';
 // import {a,b} from './Products'; // destructuring in JS
 function App() {
   return (
@@ -9,6 +10,14 @@ function App() {
       <header>
         <NavBar />
       </header>
+      <TypeWriter texts = {[
+        "Software Developer",
+        "ML Research Intern at IIT Bombay",
+        "Aspiring Computer Scientist"
+      ]}
+      typingSpeed= {100}
+      deletingSpeed = {50}
+      pauseDuration = {1000}/>
       <Effect />
       <Products />
     </div>
