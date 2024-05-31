d01d08bfb8f0f6e12c52e30ed248db1355100f2c
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2024-05-31 09:55:07 +0530

    added tile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Effect from './components/Effect/Effect';
 import Products from './components/Products/Products';
 import NavBar from './components/NavBar/NavBar';
 import TypeWriter from './components/Effect/TypeWriter';
+import Tile from './components/Tile/Tile';
 // import {a,b} from './Products'; // destructuring in JS
 function App() {
   return (
@@ -18,6 +19,15 @@ function App() {
       typingSpeed= {100}
       deletingSpeed = {50}
       pauseDuration = {1000}/>
+      <Tile
+        header = "About Me"
+        subHeader = "I am a Software Developer"
+        img = "https://cdn.dribbble.com/users/3484830/screenshots/16787618/media/b134e73ef667b926c76d8ce3f962dba2.gif"
+        text = "I am a Software Developer with a passion for developing scalable applications. I am currently working as a Machine Learning Research Intern at IIT Bombay. I am an aspiring Computer Scientist with a keen interest in developing efficient algorithms."
+        link = "#"
+        linkText = "Read More"
+        position = "img-right"
+      />
       <Effect />
       <Products />
     </div>
