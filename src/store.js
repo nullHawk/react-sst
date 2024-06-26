f4f15c72dd271961f89bbc445bf4d669fcc797ad
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2024-06-26 11:27:38 +0530

    code refactor

--- src/store.js
+++ src/store.js
@@ -1,5 +1,22 @@
 import {createStore} from "redux";
 
+const ADD_TO_CART = "ADD_TO_CART_ONE";
+const REMOVE_FROM_CART = "ADD_TO_CART";
+
+export function addToCart(product){
+    return{
+        type: ADD_TO_CART,
+        payload: product
+    }
+}
+
+export function removeFromCart(product){
+    return{
+        type: REMOVE_FROM_CART,
+        payload: product
+    }
+}
+
 function cartReducer(state={items:{}},action){
     switch(action.type){
         case "ADD_TO_CART":
